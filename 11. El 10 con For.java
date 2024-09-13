7f347fce34e7aaebf6badbce8727aef38dca99f8
Juan Ignacio Ferrando Villoslada <juaniferrando2007@gmail.com>
2024-09-13 15:43:37 -0300

    Adding ponit 11

---
+++ 11. El 10 con For.java
@@ -0,0 +1,11 @@
+//  Haz el mismo ejercicio anterior con un bucle for.
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  
+  for (int value = 1; value <= 100; value++){
+    System.out.println(value);
+  }
+  }
+}
